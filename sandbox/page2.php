bb6a5e9ffb068ed7007dfcf3789d58805ca3e17b
rossluebe <ross@athleticsnyc.com>
2015-05-21 18:18:04 -0400

    Sandbox files for testing.

---
+++ sandbox/page2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php include_once 'includes/nav.html'; ?>
+
+	<div class="page_content_holder">
+		<!-- start:page_content -->
+		<div class="content" data-config="">
+
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/Dd7FixvoKBw" frameborder="0" allowfullscreen></iframe>
+
+		</div>
+		<!-- end:page_content -->
+	</div>
+
+	<?php include_once 'includes/scripts.html'; ?>
+
+</body>
+</html>
