51908961cb186f003a65dc6c83a142561371b0bb
Matthew Spencer <matthew.spencer@athleticsnyc.com>
2015-06-19 20:07:08 -0400

    Add Util module comments

--- dist/StateManager.js
+++ dist/StateManager.js
@@ -5,10 +5,7 @@
  * @see https://github.com/athletics/StateManager
  * @version 0.1.0
  *//**
- * Util.js
- * @param  {[type]} window    [description]
- * @param  {[type]} factory [description]
- * @return {[type]}         [description]
+ * Shared utilities.
  */
 ( function ( window, factory ) {
 
@@ -52,24 +49,32 @@
     //////////////////////////////////////////////////////////////////////////////////////
 
     /**
-     * Debug - console.log wrapper
+     * A console.log wrapper with the correct line numbers.
      *
-     * @param mixed obj
+     * @see    https://gist.github.com/bgrins/5108712
+     * @see    https://developer.mozilla.org/en-US/docs/Web/API/Console/log
+     * @param  {Mixed}
+     * @return {String}
      */
-    debug = function () {
+    var debug = function () {
 
-        if ( ! debugEnabled ) {
-            return;
-        } else if ( typeof console !== 'object' || ! console.log ) {
+        if ( typeof console !== 'object' || ! console.log ) {
             return;
         }
 
-        return Function.prototype.bind.call( console.log, console );
+        return console.log.bind( console );
 
     } ();
 
-    //////////////////////////////////////////////////////////////////////////////////////
-
+    /**
+     * Get a full url from a relative url.
+     *
+     * @todo   Remove hardcoded localhost URLs.
+     * @todo   Remove bypass.
+     *
+     * @param  {String} url
+     * @return {String} url
+     */
     function fullyQualifyUrl( url ) {
 
         var bypass = true;
@@ -80,7 +85,7 @@
 
         if ( url.indexOf( 'http://' ) === -1 &&
             url.indexOf( 'http://localhost:8000/' ) === -1 &&
-            url.substring( 0, 1 ) === '/') {
+            url.substring( 0, 1 ) === '/' ) {
 
             url = 'http://localhost:8000/' + url;
 
@@ -98,35 +103,38 @@
 
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////
-
+    /**
+     * Sanitize and set the document title.
+     *
+     * @param {String} input
+     */
     function setDocumentTitle( input ) {
 
         var entities = {
-            '&amp;': '&',
-            '&lt;': '<',
-            '&gt;': '>',
-            '&#8211;': '–', //en dash
-            '&#8212;': '—', //em dash
-            '&#8216;': '‘', //left single quotation mark
-            '&#8217;': '’', //right single quotation mark
-            '&#8220;': '“', //left double quotation mark
-            '&#8221;': '”', //right double quotation mark
-            '&#8226;': '•', //bullet
-            '&#8230;': '…'  //ellipsis
+            '&amp;':   '&',
+            '&lt;':    '<',
+            '&gt;':    '>',
+            '&#8211;': '–', // en dash
+            '&#8212;': '—', // em dash
+            '&#8216;': '‘', // left single quotation mark
+            '&#8217;': '’', // right single quotation mark
+            '&#8220;': '“', // left double quotation mark
+            '&#8221;': '”', // right double quotation mark
+            '&#8226;': '•', // bullet
+            '&#8230;': '…'  // ellipsis
         };
 
-        for ( var prop in entities ) {
-            if ( entities.hasOwnProperty( prop ) ) {
-                input = input.replace( new RegExp( prop, 'g' ), entities[prop] );
+        for ( var entity in entities ) {
+            if ( ! entities.hasOwnProperty( entity ) ) {
+                continue;
             }
+
+            input = input.replace( new RegExp( entity, 'g' ), entities[ entity ] );
         }
 
         document.title = input;
 
     }
-
-    //////////////////////////////////////////////////////////////////////////////////////
 
     /**
      * Does the browser support window.history and push state.
@@ -146,10 +154,10 @@
     //////////////////////////////////////////////////////////////////////////////////////
 
     return {
-        debug: debug,
-        fullyQualifyUrl: fullyQualifyUrl,
+        debug:            debug,
+        fullyQualifyUrl:  fullyQualifyUrl,
         setDocumentTitle: setDocumentTitle,
-        getMode: getMode
+        getMode:          getMode
     };
 
 } ) );
